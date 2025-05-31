a0546a23c6e76d906344f1fa912bb6b655f86637
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2025-05-31 20:49:21 -0300

    FadaIA Projectile and Walk

---
+++ Scripts/projetil.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var speed: float = 100
+@export var direction = Vector2.UP
+
+func _physics_process(delta: float) -> void:
+	position += direction * speed * delta
+
+func setDirection(Direction: Vector2) -> void:
+	direction = Direction
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		if body.has_method("take_damage"):
+			body.take_damage(1)
