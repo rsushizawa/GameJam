95ad57bc0db8d222c181fd32518e62a9f418eb0f
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2025-05-31 22:17:48 -0300

    Portais Funcionando

---
+++ Scripts/portal.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export_file("*.tscn") var next_level_scene_path: String
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		go_to_next_level()
+
+func go_to_next_level():
+	if next_level_scene_path and not next_level_scene_path.is_empty():
+		print("Loading next level: " + next_level_scene_path)
+		get_tree().change_scene_to_file(next_level_scene_path)
